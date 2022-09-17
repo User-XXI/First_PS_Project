c57059d5228e4d6a9c8c7b757f13b302071f0097
User-XXI <91723070+User-XXI@users.noreply.github.com>
2022-09-17 23:45:50 +0300

    Add new sum

---
+++ SumProject.ps1
@@ -0,0 +1,14 @@
+﻿if($args.Count -ge 1)
+{
+    for($count = 0; $args.Count -gt $count; $count++)
+    {
+        $result += $args[$count]
+    }
+    $result > C:\Users\Andrey\Desktop\PS\SumResult.txt
+    $result >> C:\Users\Andrey\Desktop\PS\SumResultLog.txt
+    $result
+}
+else
+{
+    Write-Host "ERROR: Few arguments" -ForegroundColor Red
+}
